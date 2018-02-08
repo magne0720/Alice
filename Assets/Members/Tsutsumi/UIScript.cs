f4997cff5f758f437dbcce63756fa1e09aa4384b
tsutsumitoshiki <nkc.toshiki.tsutsumi@gmail.com>
2018-02-08 17:18:39 +0900

    myScene、HPバー、エネミー実験用、WAVE実験中、追加

---
+++ Assets/Members/Tsutsumi/UIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIScript : MonoBehaviour {
+
+    public EnemySc EnemyScript;
+
+    public Slider slider;
+    public Text HPnum;
+
+
+	// Use this for initialization
+	void Start () {
+        slider.maxValue = EnemyScript.EnemyHP;
+        HPnum.text = EnemyScript.EnemyHP.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        HPbar();
+
+    }
+
+    //EnemyのHPゲージと数字
+    void HPbar()
+    {
+
+            slider.value = EnemyScript.EnemyHP;
+            HPnum.text = EnemyScript.EnemyHP.ToString();
+    }
+}
---
+++ Assets/Members/Tsutsumi/WaveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveScript : MonoBehaviour {
+
+    public Text Wave;
+    int WaveCount = 1;
+
+	// Use this for initialization
+	void Start () {
+        Wave.text = ("WAVE  ") + WaveCount.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
