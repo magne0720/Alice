96ae361b7cc4f1da2362c1c1888767153cdaebf0
野津 正太郎 <nkc.syoutarou.nozu@gmail.com>
2018-02-20 10:34:20 +0900

    BulletManagerの基底クラスをBulletに変更。

--- Assets/Resources/Scripts/Bullets/BulletManager.cs
+++ Assets/Resources/Scripts/Bullets/BulletManager.cs
@@ -1,78 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace master
+
+public class BulletManager : Bullet
 {
-    public class BulletManager : MonoBehaviour
+    public List<GameObject> Mother = new List<GameObject>();
+    // Use this for initialization
+
+    void Start()
     {
-        public List<GameObject> Mother = new List<GameObject>();
-        // Use this for initialization
+        Mother = GetAll(gameObject);
+    }
 
-        void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        int count = 0;
+        foreach (GameObject g in Mother)
         {
-            Mother = GetAllChildren.GetAll(gameObject);
-
+            if (g == null)
+            {
+                Mother.RemoveAt(count);
+            }
+            count++;
+            continue;
         }
 
-        // Update is called once per frame
-        void Update()
+        if (Mother.Count == 0)
         {
-            int count = 0;
-            foreach (GameObject g in Mother)
-            {
-                if (g == null)
-                {
-                    Mother.RemoveAt(count);
-                }
-                count++;
-            }
-
-            if (Mother.Count == 0)
-            {
-                Destroy(gameObject);
-            }
+            Destroy(gameObject);
         }
     }
-    public static class GetAllChildren
-
+    public List<GameObject> GetAll(GameObject obj)
     {
 
-        public static List<GameObject> GetAll(this GameObject obj)
+        List<GameObject> allChildren = new List<GameObject>();
 
+        GetChildren(obj, ref allChildren);
+
+        return allChildren;
+
+    }
+    //子要素を取得してリストに追加
+
+    public void GetChildren(GameObject obj, ref List<GameObject> allChildren)
+    {
+
+        Transform children = obj.GetComponentInChildren<Transform>();
+
+        //子要素がいなければ終了
+
+        if (children.childCount == 0)
         {
-
-            List<GameObject> allChildren = new List<GameObject>();
-
-            GetChildren(obj, ref allChildren);
-
-            return allChildren;
-
+            return;
         }
+        Debug.Log("layer=" + gameObject.layer);
 
-
-
-        //子要素を取得してリストに追加
-
-        public static void GetChildren(GameObject obj, ref List<GameObject> allChildren)
-
+        foreach (Transform ob in children)
         {
-
-            Transform children = obj.GetComponentInChildren<Transform>();
-
-            //子要素がいなければ終了
-
-            if (children.childCount == 0)
-            {
-
-                return;
-
-            }
-
-            foreach (Transform ob in children)
-            {
-                allChildren.Add(ob.gameObject);
-                GetChildren(ob.gameObject, ref allChildren);
-            }
+            ob.gameObject.layer = gameObject.layer;
+            ob.gameObject.tag = gameObject.tag;
+            allChildren.Add(ob.gameObject);
+            GetChildren(ob.gameObject, ref allChildren);
         }
     }
 }
